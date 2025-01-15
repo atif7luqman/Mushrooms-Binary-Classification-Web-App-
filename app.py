aadaabab943bd2d7358c5fc18925d13997185585
atif7luqman <atifluqman28@gmail.com>
2025-01-15 14:52:16 +0500

    Make dataset path relative

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def main():
 
     @st.cache_data
     def load_data():
-        data = pd.read_csv("D:\\Data Science\\DeepLearning\\streamlit-ml\\mushrooms.csv")
+        data = pd.read_csv("mushrooms.csv")
         labelencoder = LabelEncoder()
         for col in data.columns:
             data[col] = labelencoder.fit_transform(data[col])
